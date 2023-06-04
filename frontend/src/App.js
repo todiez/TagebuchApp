55e151eded7e711f1709496dbf77c7cfadfdf0b0
todiez <tobias@diez.guru>
2023-06-04 21:54:16 +0200

    frontend changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,102 @@
-import React, { Component } from 'react'
-import './App.css'
-class App extends Component {
-  state = {
-    cow: '',
-    text: ''
-  }
-componentDidMount() {
-    this.fetchCow()
-  }
-fetchCow = async () => {
-    const response = await fetch(`/api/cow`)
-    const initialCow = await response.json()
-    const cow = initialCow.moo
-    this.setState({ cow })
-  }
-customCow = async evt => {
-    evt.preventDefault()
-    const text = this.state.text
-    const response = await fetch(`/api/cow/${text}`)
-    const custom = await response.json()
-    const cow = custom.moo
-    this.setState({ cow, text: '' })
-  }
-handleChange = evt => {
-    this.setState({ [evt.target.name]: evt.target.value })
-    console.log(this.state.text)
-  }
-render() {
-    return (
-      <div className="App">
-        <h3>Text Cow. Moo</h3>
-        <code>{this.state.cow}</code>
-        <form onSubmit={this.customCow}>
-          <label>Custom Cow Text:</label>
-          <input
-            type="text"
-            name="text"
-            value={this.state.text}
-            onChange={this.handleChange}
-          />
-          <button type="submit">Show me a talking cow!</button>
-        </form>
-      </div>
-    )
-  }
+
+
+
+import { useState } from 'react'
+
+const test = process.env.REACT_APP_TESTVAR;
+
+function App() {
+
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const handleOnSubmit = async (e) => {
+		e.preventDefault();
+		let result = await fetch(
+		'http://localhost:5000/register', {
+			method: "post",
+			body: JSON.stringify({ name, email }),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+		result = await result.json();
+		console.warn(result);
+		if (result) {
+			alert("Data saved succesfully");
+			setEmail("");
+			setName("");
+		}
+	}
+	return (
+		<>
+			<h1>This is React WebApp, let's create a Tagebuch for Schorschy</h1>
+			<form action="">
+				<input type="text" placeholder="name"
+				value={name} onChange={(e) => setName(e.target.value)} />
+				<input type="email" placeholder="email"
+				value={email} onChange={(e) => setEmail(e.target.value)} />
+				<button type="submit"
+				onClick={handleOnSubmit}>submit</button>
+			</form>
+			<h2>{test}</h2>
+
+		</>
+	);
 }
-export default App
+
+export default App;
+
+
+
+
+
+
+
+// import React, { Component } from 'react'
+// import './App.css'
+// class App extends Component {
+//   state = {
+//     cow: '',
+//     text: ''
+//   }
+// componentDidMount() {
+//     this.fetchCow()
+//   }
+// fetchCow = async () => {
+//     const response = await fetch(`/api/cow`)
+//     const initialCow = await response.json()
+//     const cow = initialCow.moo
+//     this.setState({ cow })
+//   }
+// customCow = async evt => {
+//     evt.preventDefault()
+//     const text = this.state.text
+//     const response = await fetch(`/api/cow/${text}`)
+//     const custom = await response.json()
+//     const cow = custom.moo
+//     this.setState({ cow, text: '' })
+//   }
+// handleChange = evt => {
+//     this.setState({ [evt.target.name]: evt.target.value })
+//     console.log(this.state.text)
+//   }
+// render() {
+//     return (
+//       <div className="App">
+//         <h3>Text Cow. Moo</h3>
+//         <code>{this.state.cow}</code>
+//         <form onSubmit={this.customCow}>
+//           <label>Custom Cow Text:</label>
+//           <input
+//             type="text"
+//             name="text"
+//             value={this.state.text}
+//             onChange={this.handleChange}
+//           />
+//           <button type="submit">Show me a talking cow!</button>
+//         </form>
+//       </div>
+//     )
+//   }
+// }
+// export default App
--- index.js
+++ index.js
@@ -1,3 +1,47 @@
+// To connect with your mongoDB database
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+
+const connectDB = async () => {
+  try {
+      mongoose.set('strictQuery', false);
+      const conn = await mongoose.connect(process.env.MONGOURL);
+      console.log(`Database connected: ${conn.connection.host}`);
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+connectDB();
+
+
+//module.exports = connectDB;
+
+// Schema for users of app
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("users", UserSchema);
+User.createIndexes();
+
+
+
+
+
 const express = require('express')
 const cowsay = require('cowsay')
 const cors = require('cors')
@@ -9,26 +53,28 @@ const app = express()
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, 'frontend/build')))
 
-// Serve our api route /cow that returns a custom talking text cow
-app.get('/api/cow/:say', cors(), async (req, res, next) => {
-  try {
-    const text = req.params.say
-    const moo = cowsay.say({ text })
-    res.json({ moo })
-  } catch (err) {
-    next(err)
-  }
-})
+// // Serve our api route /cow that returns a custom talking text cow
+// app.get('/api/cow/:say', cors(), async (req, res, next) => {
+//   try {
+//     const text = req.params.say
+//     const moo = cowsay.say({ text })
+//     res.json({ moo })
+//   } catch (err) {
+//     next(err)
+//   }
+// })
 
-// Serve our base route that returns a Hellow World cow
-app.get('/api/cow/', cors(), async (req, res, next) => {
-  try {
-    const moo = cowsay.say({ text: 'Hello World!' })
-    res.json({ moo })
-  } catch (err) {
-    next(err)
-  }
-})
+// // Serve our base route that returns a Hellow World cow
+// app.get('/api/cow/', cors(), async (req, res, next) => {
+//   try {
+//     const moo = cowsay.say({ text: 'Hello World!' })
+//     res.json({ moo })
+//   } catch (err) {
+//     next(err)
+//   }
+// })
+
+
 
 // Anything that doesn't match the above, send back the index.html file
 app.get('*', (req, res) => {
