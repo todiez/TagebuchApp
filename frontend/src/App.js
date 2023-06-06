89823c886b7760a4b89410168173ec4105dc0ad6
todiez <tobias@diez.guru>
2023-06-06 11:10:24 +0200

    testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,101 +1,107 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const test = process.env.REACT_APP_TESTVAR;
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
-	const [name, setName] = useState("");
-	const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  async function handle() {
+    // Email validation logic
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      alert("Invalid email address");
+      return;
+    }
 
-	  const [password, setPassword] = useState('');
-	  async function handle() {
-		// Email validation logic
-		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-		if (!emailRegex.test(email)) {
-		  alert("Invalid email address");
-		  return;
-		}
-	  
-		const url = `/account/create/${name}/${email}/${password}`;
-		try {
-		  const res = await fetch(url);
-		  const data = await res.json();
-		  console.log(data);
-		} catch (error) {
-		  alert("Email address is already in use");
-		  return;
-		}
-	  }
-	
+    const url = `/account/create/${name}/${email}/${password}`;
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      alert("Email address is already in use");
+      return;
+    }
+  }
 
-
-	const handleOnSubmit = async (e) => {
-		e.preventDefault();
-		let result = await fetch(
-		'/register', {
-			method: "post",
-			body: JSON.stringify({ name, email }),
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		})
-		result = await result.json();
-		console.warn(result);
-		if (result) {
-			alert("Data saved succesfully");
-			setEmail("");
-			setName("");
-		}
-	}
-	return (
-		<>
-			<h1>This is React WebApp, let's create a Tagebuch for Schorschy</h1>
-			<form action="">
-				<input type="text" placeholder="name"
-				value={name} onChange={(e) => setName(e.target.value)} />
-				<input type="email" placeholder="email"
-				value={email} onChange={(e) => setEmail(e.target.value)} />
-				<button type="submit"
-				onClick={handleOnSubmit}>submit</button>
-			</form>
-			<h2>{test}</h2>
-
-			Name<br/>
-    <input type="input" 
-      className="form-control" 
-      placeholder="Enter name" 
-      value={name} 
-      onChange={e => setName(e.currentTarget.value)} /><br/>
-
-    Email address<br/>
-    <input type="input" 
-      className="form-control" 
-      placeholder="Enter email" 
-      value={email} 
-      onChange={e => setEmail(e.currentTarget.value)}/><br/>
-
-    Password<br/>
-    <input type="password" 
-      className="form-control" 
-      placeholder="Enter password" 
-      value={password} 
-      onChange={e => setPassword(e.currentTarget.value)}/><br/>
-
-    <button type="submit" 
-      className="btn btn-dark" 
-      onClick={handle}>Create Account</button>
-
-		</>
-	);
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    let result = await fetch("/register", {
+      method: "post",
+      body: JSON.stringify({ name, email }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    result = await result.json();
+    console.warn(result);
+    if (result) {
+      alert("Data saved succesfully");
+      setEmail("");
+      setName("");
+    }
+  };
+  return (
+    <>
+      <h1>This is React WebApp, let's create a Tagebuch for Schorschyy</h1>
+      <form action="">
+        <input
+          type="text"
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit" onClick={handleOnSubmit}>
+          submit
+        </button>
+      </form>
+      <h2>{test}</h2>
+      Name
+      <br />
+      <input
+        type="input"
+        className="form-control"
+        placeholder="Enter name"
+        value={name}
+        onChange={(e) => setName(e.currentTarget.value)}
+      />
+      <br />
+      Email address
+      <br />
+      <input
+        type="input"
+        className="form-control"
+        placeholder="Enter email"
+        value={email}
+        onChange={(e) => setEmail(e.currentTarget.value)}
+      />
+      <br />
+      Password
+      <br />
+      <input
+        type="password"
+        className="form-control"
+        placeholder="Enter password"
+        value={password}
+        onChange={(e) => setPassword(e.currentTarget.value)}
+      />
+      <br />
+      <button type="submit" className="btn btn-dark" onClick={handle}>
+        Create Account
+      </button>
+    </>
+  );
 }
 
 export default App;
-
-
-
-
-
-
 
 // import React, { Component } from 'react'
 // import './App.css'
